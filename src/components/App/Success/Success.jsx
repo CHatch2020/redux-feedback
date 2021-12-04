4c08761a08d4e0f46d45ed617ba2a7257ca0ad15
Caleb Hatch <hatchcaleb@gmail.com>
2021-12-04 17:58:22 -0600

    got success component working

--- src/components/App/Success/Success.jsx
+++ src/components/App/Success/Success.jsx
@@ -1,9 +1,21 @@
-
+import React from 'react';
+import {useHistory} from 'react-router-dom';
 
 function Success(){
+    const history = useHistory();
+
+    const goBackHome = () => {
+        console.log('Niner, Niner, Enroute back to mother planet');
+        history.push('/');
+    }
+
     return(
         <div>
+            <h2>Feedback!</h2>
+            <h4>Rate App: </h4>
 
+            <h3>Thank You!</h3>
+            <button onClick={goBackHome}>Leave New Feedback</button>
         </div>
     );
 };
